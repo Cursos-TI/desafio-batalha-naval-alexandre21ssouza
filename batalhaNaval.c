4d83aa2bb622371ebc05ed52b553854fa7ba92de
Alexandre Barros <103068697+alexandre21ssouza@users.noreply.github.com>
2025-04-05 20:05:26 +0000

    Nível novato completo

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -9,6 +9,55 @@ int main() {
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    
+    //Construção da matriz
+    int tabuleiro[10][10];
+
+    //vetores com os navios na horizontal e vertical
+    int navioHorizontal_1[3] ={3,3,3};
+    int navioVertical_1[3] ={3,3,3};
+    
+    //Direcionando a posição do navioHorizontal_1
+    int linha_1 = 1;
+    int coluna_1 = 3;
+
+    //Direcionando a posição do navioVertical_1
+    int linha_2 = 7;
+    int coluna_2 = 9;
+
+
+    
+    //Inicializando a matriz com 0
+    for (int i = 0; i < 10; i++){
+        for (int j = 0; j < 10; j++){
+           tabuleiro[i][j] = 0;
+
+        }
+
+    }
+
+    //Posicionando navioHorizontal_1 na matriz
+    for (int i = 0; i < 3; i++){
+        //coluna_1 + i, distribui o numero 3 nas posições das colunas
+        tabuleiro[linha_1][coluna_1 + i] = navioHorizontal_1[i];
+    }
+    
+
+    ////Posicionando navioVertical_1 na matriz
+    for (int i = 0; i < 3; i++){
+        //linha_2 + i, distribui o numero 3 nas posições das linhas
+        tabuleiro[linha_2 +i][coluna_2] = navioVertical_1[i];
+    }
+    
+    //Impressão do jogo batalha naval com as posições criadas dos navios
+    printf("-------------------Batalha Naval------------------\n\n\n");
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
